3ddc2f2903890499238ee5e6b5b36e134b8e20d1
Vineeth NC <ncvineeth97@gmail.com>
2022-02-04 19:41:49 -0500

    Pushed solutions for 0027

---
+++ codes/0027_remove_element.py
@@ -0,0 +1,8 @@
+class Solution:
+	def removeElement(self, nums: List[int], val: int) -> int:
+		counter = 0
+		for i in range(0, len(nums)):
+			if nums[i] != val:
+				nums[counter] = nums[i]
+				counter += 1
+		return counter
